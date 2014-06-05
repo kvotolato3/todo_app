2444cfdbe4f70f75ff924fbe5bbc42ceb54d234a
Kathleen Votolato <kvotolato3@gmail.com>
2014-06-05 07:49:05 -0400

    Removed functions that are no longer needed:
    - listElement
    - completeButton
    - deleteButton

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -24,24 +24,6 @@ var TodoApp = {
       $('#unfinished-list').append(todo_item_html);
     }
   },
-
-  listElement: function(item){
-    newElement = $('<tr>').addClass('list-item').attr('id', item.id) ;
-    newElement.html(item.created_at + " | " + item.text + " | ").append(TodoApp.completeButton()).append(TodoApp.deleteButton());
-    return newElement;
-  },
-
-  completeButton: function(){
-    var attributes = {'id':"complete-item-button"};
-    var newElement = $('<button>').addClass("btn btn-small btn-default").attr(attributes).text("Complete");
-    return newElement;
-  },
-
-  deleteButton: function(){
-    var attributes = {'id':"delete-item-button"};
-    var newElement = $('<button>').addClass("btn btn-small btn-default").attr(attributes).text("Delete");
-    return newElement;
-  },
   addNewItem: function(){
     var $itemInput = $('#new-item').val();
     if ($itemInput!== '') {
